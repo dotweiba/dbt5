4e81fa5854c6a04845015ef3f3b76a58bb67eb21
Wei WEI <dotweiba@gmail.com>
2020-04-26 22:57:34 +0000

    Add more denpendencies and change the example command to run workload.

--- run_pgsql_dbt5.sh
+++ run_pgsql_dbt5.sh
@@ -4,7 +4,7 @@ basedir=`pwd`
 
 # Building & installing DBT5
 sudo apt update
-sudo apt-get install -y  build-essential autoconf cmake
+sudo apt install -y build-essential autoconf cmake r-base bc sysstat ghostscript
 sudo apt install -y postgresql-server-dev-all libpqxx-dev postgresql libpq-dev
 cd $basedir && cmake CMakeLists.txt -DDBMS=pgsql
 cd $basedir/egen/prj && make -f Makefile.pgsql
@@ -16,8 +16,8 @@ sudo cp -R $basedir/../dbt5/ /opt/
 sudo chown -R postgres:postgres /opt/dbt5
 
 # Building DB
-sudo -u postgres /bin/bash -c "source /opt/dbt5/dbt5_pgsql_profile && dbt5-pgsql-build-db -l 5433 -c 5000 -t 5000 -s 500 -w 1"
+sudo -u postgres /bin/bash -c "source /opt/dbt5/dbt5_pgsql_profile && dbt5-pgsql-build-db -c 5000 -t 5000 -s 500 -w 1"
 
 # Running Workload
-sudo -u postgres /bin/bash -c "source /opt/dbt5/dbt5_pgsql_profile && rm -rf /tmp/results_dbt5 && dbt5-run-workload -a pgsql -f 1 -o /tmp/results_dbt5 -n dbt5 -c 100 -t 10000 -u 5 -d 100 -w 100 -p 5433"
+sudo -u postgres /bin/bash -c "source /opt/dbt5/dbt5_pgsql_profile && rm -rf /tmp/results_dbt5 && dbt5-run-workload -a pgsql -f 1 -o /tmp/results_dbt5 -n dbt5 -c 1000 -t 10000 -u 5 -d 100 -w 100"
 
